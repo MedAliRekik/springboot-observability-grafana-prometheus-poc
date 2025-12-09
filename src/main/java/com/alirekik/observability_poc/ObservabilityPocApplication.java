d9093e35f260af7219c7a37e122a0f0e8e7bcff5
Ali REKIK <medali.rekik.ing@gmail.com>
2025-12-10 00:55:54 +0100

    Initial Commit

---
+++ src/main/java/com/alirekik/observability_poc/ObservabilityPocApplication.java
@@ -0,0 +1,13 @@
+package com.alirekik.observability_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityPocApplication.class, args);
+	}
+
+}
